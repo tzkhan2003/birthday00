8baedd05f70c23ef0006f00f595a60f2451b02eb
Tanjim Zaman Khan <67259158+tzkhan2003@users.noreply.github.com>
2022-06-11 01:08:31 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'jun 12, 2022',
+  birthdate: 'Jun 12, 2022',
   name: 'Nabila'
 };
 
